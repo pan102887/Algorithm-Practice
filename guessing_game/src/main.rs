13b87e3d8b52034281e3f248c7d26a1dc25d8b99
jupeter <pan102887@qq.com>
2020-12-20 01:20:43 +0800

    thie game of guessing number by rust

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,13 +1,36 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("please inputs the number.");
-    let mut geuss = String::new();
+    let  true_number = rand::thread_rng().gen_range(1,102);
 
+    
+    
+    loop {
+        
+        let mut guess = String::new();
 
+        println!("please input your guess");
 
-    io::stdin()
-        .read_line(&mut geuss)
-        .expect("Failed to read line");
-    println!("your input is: {}", geuss);
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 =match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&true_number) {
+            Ordering::Equal => {
+                println!("great");
+                break;
+            },
+            Ordering::Less => println!("too small"),
+            Ordering::Greater => println!("too big"),
+        }
+    }
+    println!("the true_number is {}",true_number);
 }
+ 
