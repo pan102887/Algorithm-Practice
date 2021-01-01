471bc7e6036447ff03879de977b7b5be197de019
jupeter <pan102887@qq.com>
2021-01-01 13:48:28 +0800

    the ownership

--- length_of_longest_substring/src/main.rs
+++ length_of_longest_substring/src/main.rs
@@ -1,8 +1,17 @@
 fn main() {
+    let s = String::from("hello");
+    takes_ownewship(s);
+
+    let x = 5;
+    makes_copy(x);
+
+    println!("{}",s) //this syntax is illegal beacause the function takes_ownewship take the ownership of the string s
 
 }
 
-
-fn length_of_longest_substring(s : str) -> i32{
-    
+fn takes_ownewship(s: String){
+    println!("{}",s);
+}
+fn makes_copy(i:i32){
+    println!("{}",i);
 }
